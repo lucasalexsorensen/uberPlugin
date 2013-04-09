1b1132ee5bffbe468e298e42a2a369a2ab32656c
Lucas Alexander Sorensen <lasdeveloper@gmail.com>
2013-04-09 15:16:20 +0200

    Changed some more

--- src/uberslayer/Reference.java
+++
@@ -1,7 +0,0 @@
-package uberslayer;
-
-public class Reference {
-	
-	public static final String PLUGIN_VERSION = "0.0.1";
-	public static final String PLUGIN_NAME = "UberPlugin";
-}
--- src/uberslayer/UberPlugin.java
+++ src/uberslayer/UberPlugin.java
@@ -1,26 +1,35 @@
 package uberslayer;
 
-import org.bukkit.Material;
-import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.ShapedRecipe;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class UberPlugin extends JavaPlugin{
-	
+public class UberPlugin extends JavaPlugin implements Listener{
+
 	@Override
 	public void onEnable(){
 		getLogger().info("UberPlugin has been enabled!");
-		getLogger().info("UberPlugin is initializing..");
-		getCommand("uber").setExecutor(new UberPluginCommandExecutor(this));
 		
-		ShapedRecipe lavaBucket = new ShapedRecipe(new ItemStack(Material.LAVA_BUCKET)).shape("***","*%*","***").setIngredient('*', Material.NETHER_BRICK).setIngredient('%', Material.WATER_BUCKET);
-
-		getServer().addRecipe(lavaBucket);
+		PluginManager pm = getServer().getPluginManager();
+		
+		pm.registerEvents(this, this);
+		
 	}
 	
-	@Override
-	public void onDisable(){
-		getLogger().info("UberPlugin has been disabled!");
+	@EventHandler(ignoreCancelled = true)
+	public void onBlockBreak(BlockBreakEvent event){
+		Player plr = event.getPlayer();
+		plr.sendMessage(ChatColor.GOLD + "You broke a " + event.getBlock().getType());
 	}
-
+	
+	@EventHandler(ignoreCancelled = true)
+	public void onBlockPlace(BlockPlaceEvent event){
+		Player plr = event.getPlayer();
+		plr.sendMessage(ChatColor.GOLD + "You placed a " + event.getBlock().getType());
+	}
 }
