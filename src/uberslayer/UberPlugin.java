e44655cd7c1a3e3f8f7e68148ec6b52ef1ebf181
Lucas Alexander Sorensen <lasdeveloper@gmail.com>
2013-04-09 09:40:39 +0200

    Changed some stuff

--- src/uberslayer/UberPlugin.java
+++ src/uberslayer/UberPlugin.java
@@ -1,5 +1,8 @@
 package uberslayer;
 
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.ShapedRecipe;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class UberPlugin extends JavaPlugin{
@@ -7,12 +10,17 @@ public class UberPlugin extends JavaPlugin{
 	@Override
 	public void onEnable(){
 		getLogger().info("UberPlugin has been enabled!");
+		getLogger().info("UberPlugin is initializing..");
 		getCommand("uber").setExecutor(new UberPluginCommandExecutor(this));
+		
+		ShapedRecipe lavaBucket = new ShapedRecipe(new ItemStack(Material.LAVA_BUCKET)).shape("***","*%*","***").setIngredient('*', Material.NETHER_BRICK).setIngredient('%', Material.WATER_BUCKET);
+
+		getServer().addRecipe(lavaBucket);
 	}
 	
 	@Override
 	public void onDisable(){
-		getLogger().info("UberPlugin has been disabled! :(");
+		getLogger().info("UberPlugin has been disabled!");
 	}
 
 }
