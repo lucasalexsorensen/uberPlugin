b54be44a12949b8435cd33b900d5005a37300c87
Lucas Alexander Sorensen <lasdeveloper@gmail.com>
2013-04-09 20:58:11 +0200

    Made it so iron bars drop 1 iron ingot
    Just testing how to add block drop listeners and stuffs

--- src/uberslayer/UberPlugin.java
+++ src/uberslayer/UberPlugin.java
@@ -1,10 +1,18 @@
 package uberslayer;
 
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import uberslayer.commandexecutors.CommandUberSword;
 
-public class UberPlugin extends JavaPlugin{
+public class UberPlugin extends JavaPlugin implements Listener{
 
 	@Override
 	public void onDisable(){
@@ -15,8 +23,19 @@ public class UberPlugin extends JavaPlugin{
 	public void onEnable(){
 		getLogger().info("UberPlugin has been enabled!");
 		
+		PluginManager pm = getServer().getPluginManager();
+		pm.registerEvents(this, this);
+		
 		getCommand("ubersword").setExecutor(new CommandUberSword(this));
 	}
 	
+	@EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
+	public void onBlockBreak(BlockBreakEvent event){
+		if(event.getBlock().getType() == Material.IRON_FENCE){
+			Block blk = event.getBlock();
+			blk.getDrops().clear();
+			blk.getDrops().add(new ItemStack(Material.IRON_INGOT));
+		}
+	}
 	
 }
